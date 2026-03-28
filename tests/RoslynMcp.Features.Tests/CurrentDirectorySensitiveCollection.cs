a967f599d78e8f03a365056246dffdd172cf9d39
jensaymoo <pigonthewind@pm.me>
2026-03-28 23:18:04 +0300

    remove unkown quality tests

--- tests/RoslynMcp.Features.Tests/CurrentDirectorySensitiveCollection.cs
+++
@@ -1,9 +0,0 @@
-using Xunit;
-
-namespace RoslynMcp.Features.Tests;
-
-[CollectionDefinition(Name, DisableParallelization = true)]
-public sealed class CurrentDirectorySensitiveCollection
-{
-    public const string Name = "CurrentDirectorySensitive";
-}
--- tests/RoslynMcp.Features.Tests/SharedSandboxContext.cs
+++
@@ -1,7 +0,0 @@
-namespace RoslynMcp.Features.Tests;
-
-public sealed class SharedSandboxContext : SandboxContext
-{
-    internal Task InitializeAsync(CancellationToken cancellationToken = default)
-        => InitializeSandboxAsync(TestSolutionSandbox.Create(CanonicalTestSolutionDirectory), cancellationToken);
-}
